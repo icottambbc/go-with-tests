7845f9d31083c9fd55a290728138cb1c8172e8ab
icottambbc <ian.cottam@bbc.co.uk>
2022-08-10 11:32:10 +0100

    A refactor

--- hello_test.go
+++ hello_test.go
@@ -4,11 +4,24 @@ import "testing"
 
 func TestMain(t *testing.T) {
 
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("say hello to someone", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
+		assetCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello to the world", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assetCorrectMessage(t, got, want)
+	})
+}
+
+func assetCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
-
 }
